bda2ada3ed78b46c661a8ced04a073d20fa57baa
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-03 12:00:20 -0300

    adicionei o rodape

---
+++ src/componentes/Rodape.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom"
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes"
+import styles from './Rodape.module.scss';
+
+const Rodape = () => {
+
+    const participantes = useListaDeParticipantes()
+
+    const navegarPara = useNavigate()
+
+    const iniciar = () => {
+        navegarPara('/sorteio')
+    }
+
+    return (<footer className={styles["rodape-configuracoes"]}>
+        <button className={styles["botao"]} disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+        <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
+    </footer>)
+}
+
+export default Rodape
