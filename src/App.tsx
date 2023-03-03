a79157432270db663aa7aabd6e9668da5aa00bb4
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-03 11:54:19 -0300

    troquei o elemento da route /

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import Formulario from './componentes/Formulario';
+import Configuracao from './paginas/Configuracao';
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={Formulario}/>
+          <Route path='/' element={<Configuracao />}/>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
